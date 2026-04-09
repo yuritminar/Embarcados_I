ebc2c38b0268b21f5d2706de815210930305f141
Kememm <33742638+yuritminar@users.noreply.github.com>
2026-04-09 11:45:09 -0300

    atualizações no index e no main.cpp (implementação das funções e correções pra servir a página)

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,14 @@
 #include "secrets.h"
 #include "MQUnifiedsensor.h" 
 
-
-bool modoTeste = true; //Variável que define quando eu tenho o hardware em mãos ou não
 const char* ssid = WIFI_SSID;  
 const char* password = WIFI_PASS;  
 
 AsyncWebServer server(80);
 
 /****** Fim do bloco de coniguração Wifi *******/
+
+bool modoTeste = true; //Variável que define quando eu tenho o hardware em mãos ou não
 
 /*configurações de relé*/
 bool releLigado = false;
@@ -26,18 +26,17 @@ bool releLigado = false;
 //Definições
 #define placa "Esp-32"
 #define Voltage_Resolution 3.3
-#define pin 13 //Analog input 0 of your arduino
-#define type "MQ-7" //MQ7
+#define MQ_Pin 13 //Analog input 0 of your arduino
+#define MQ_type "MQ-7" //MQ7
 #define ADC_Bit_Resolution 12 // For arduino UNO/MEGA/NANO
 #define RatioMQ7CleanAir 27.5 //RS / R0 = 27.5 ppm 
 #define PWMPin 5 // Pin connected to mosfet
 
 // Declaração do sensor
-MQUnifiedsensor MQ7(placa, Voltage_Resolution, ADC_Bit_Resolution, pin, type);
+
+/**** Bloco de configuração do MQ-7 ****/
+MQUnifiedsensor MQ7(placa, Voltage_Resolution, ADC_Bit_Resolution, MQ_Pin, MQ_type);
 unsigned long oldTime = 0;
-
-
-
 /**** Fim do bloco de configuração do MQ-7 ****/
 
 
@@ -62,6 +61,7 @@ float le_temp();
 float le_umid();
 float le_alt();
 float le_pres();
+float le_CO();
 String processor(const String& var);
 
 /*** Os protótipos abaixo existem para lidar com as variáveis de forma inteligente
@@ -77,12 +77,8 @@ bool setVent();
 bool setRele();
 /**** Fim da definição de protótipos ****/
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
   Serial.begin(9600);
  
   if (!SPIFFS.begin()) {
@@ -94,7 +90,18 @@ void setup() {
   dht22.begin();
  
   WiFi.begin(ssid, password);
+  int tentativas = 0;
 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+    tentativas++;
+  }
+  Serial.println("");
+  Serial.println("WiFi conectado!");
+  Serial.println(WiFi.localIP());
+
+  server.serveStatic("/", SPIFFS, "/");
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(SPIFFS, "/index.html", String(), false, processor);
   });
@@ -159,11 +166,86 @@ float getUmid() {
     Serial.print("Humidity: ");
     Serial.print(umid);
     Serial.print("%");
+    return umid;
   }
   }
 }
 /**** Fim das Funções do DHT22 ****/
 
+/**** Funções do MQ-7 ****/
+float le_CO() {
+  MQ7.update(); //Atualiza os valores do sensor
+  float CO = MQ7.readSensor(); //Lê o valor do sensor
+  return CO;
+}
+
+float getCO() {
+  if (modoTeste == true){
+    return 100;
+  } else {
+    float CO = le_CO();
+    Serial.print("  |  ");
+    Serial.print("CO: ");
+    Serial.print(CO);
+    Serial.println(" ppm");
+    return CO;
+  }
+}
+/**** Fim das funções do MQ-7 ****/
+
+/**** Funções do MPL3115A2 ****/
+float le_alt() {
+  float alt = baro.getAltitude();
+  return alt;
+}
+
+float le_pres() {
+  float pres = baro.getPressure();
+  return pres;
+}
+
+float getAlt() {
+  if (modoTeste == true){
+    return 500;
+  } else {
+    float alt = le_alt();
+    Serial.print("  |  ");
+    Serial.print("Altitude: ");
+    Serial.print(alt);
+    Serial.println(" m");
+    return alt;
+  }
+}
+
+float getPres() {
+  if (modoTeste == true){
+    return 1000;
+  } else {
+    float pres = le_pres();
+    Serial.print("  |  ");
+    Serial.print("Pressure: ");
+    Serial.print(pres);
+    Serial.println(" hPa");
+    return pres;
+  }
+}
+/**** Fim das funções do MPL3115A2 ****/
+
+/**** funções da ventoinha e do relé ****/
+bool setVent() {
+  // Lógica para ligar ou desligar a ventoinha
+  // Exemplo: digitalWrite(PWMPin, HIGH) para ligar, digitalWrite(PWMPin, LOW) para desligar
+  return true; // Retorna true se a operação foi bem-sucedida
+}
+
+bool setRele() {
+  // Lógica para ligar ou desligar o relé
+  // Exemplo: digitalWrite(relePin, HIGH) para ligar, digitalWrite(relePin, LOW) para desligar
+  return true; // Retorna true se a operação foi bem-sucedida
+}
+/**** Fim das funções da ventoinha e do relé ****/
+
+/**** função processor ****/
 String processor(const String& var){
   if (var == "TEMP"){
     float temp = getTemp();
@@ -173,7 +255,32 @@ String processor(const String& var){
     float umid = getUmid();
     String umidStr = String(umid, 2);
     return umidStr;
+  } else if (var == "CO") {
+    float CO = getCO();
+    String COStr = String(CO, 2);
+    return COStr;
+  } else if (var == "ALT") {
+    float alt = getAlt();
+    String altStr = String(alt, 2);
+    return altStr;
+  } else if (var == "PRES") {
+    float pres = getPres();
+    String presStr = String(pres, 2);
+    return presStr;
+  } else if (var == "VENT_STATUS") {
+    if (setVent()) {
+      return String("Ventoinha ligada/desligada com sucesso!");
+    } else {
+      return String("Falha ao ligar/desligar a ventoinha.");
+    }
+  } else if (var == "RELE_STATUS") {
+    if (setRele()) {
+      return String("Relé ligado/desligado com sucesso!");
+    } else {
+      return String("Falha ao ligar/desligar o relé.");
+    }
   } else {
     return String("Sem valor");
   }
 }
+/**** Fim da função processor ****/
